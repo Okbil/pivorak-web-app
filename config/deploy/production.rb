59d6faf75eb98060566768a4f83cbd8ded4f4048
Volodya Sveredyuk <sveredyuk@gmail.com>
2017-02-02 16:14:53 +0200

    try1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server '188.166.8.111', roles: %w{app db web}, user: 'pivorak'
-set :branch, 'development'
+set :branch, 'coming-soon'
 set :deploy_to, proc { "/home/pivorak/projects/pivorak_#{fetch :stage}" }
 set :user, 'pivorak'
